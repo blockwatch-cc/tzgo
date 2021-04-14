cca8183f76f75da7e2903eac69b7cf003028ee81
Alexander Eichhorn <alex@blockwatch.cc>
2021-04-14 13:51:15 +0400

    Add Contract and Bigmap RPC endpoints

--- rpc/contracts.go
+++ rpc/contracts.go
@@ -8,6 +8,7 @@ import (
 	"fmt"
 	"strconv"
 
+	"blockwatch.cc/tzgo/micheline"
 	"blockwatch.cc/tzgo/tezos"
 )
 
@@ -58,4 +59,101 @@ func (c *Client) GetContractBalanceHeight(ctx context.Context, addr tezos.Addres
 		return 0, err
 	}
 	return strconv.ParseInt(bal, 10, 64)
+}
+
+// GetContractScript returns the originated contract script
+func (c *Client) GetContractScript(ctx context.Context, addr tezos.Address) (*micheline.Script, error) {
+	u := fmt.Sprintf("chains/%s/blocks/head/context/contracts/%s/script", c.ChainID, addr)
+	s := micheline.NewScript()
+	err := c.Get(ctx, u, s)
+	if err != nil {
+		return nil, err
+	}
+	return s, nil
+}
+
+// GetContractStorage returns the most recent version of the contract's storage
+func (c *Client) GetContractStorage(ctx context.Context, addr tezos.Address) (micheline.Prim, error) {
+	u := fmt.Sprintf("chains/%s/blocks/head/context/contracts/%s/storage", c.ChainID, addr)
+	prim := micheline.Prim{}
+	err := c.Get(ctx, u, &prim)
+	if err != nil {
+		return micheline.InvalidPrim, err
+	}
+	return prim, nil
+}
+
+// GetContractStorage returns the contract's storage at height
+func (c *Client) GetContractStorageHeight(ctx context.Context, addr tezos.Address, height int64) (micheline.Prim, error) {
+	u := fmt.Sprintf("chains/%s/blocks/%d/context/contracts/%s/storage", c.ChainID, height, addr)
+	prim := micheline.Prim{}
+	err := c.Get(ctx, u, &prim)
+	if err != nil {
+		return micheline.InvalidPrim, err
+	}
+	return prim, nil
+}
+
+// GetContractEntrypoints returns the contract's storage at height
+func (c *Client) GetContractEntrypoints(ctx context.Context, addr tezos.Address) (map[string]micheline.Prim, error) {
+	u := fmt.Sprintf("chains/%s/blocks/head/context/contracts/%s/storage", c.ChainID, addr)
+	type eptype struct {
+		Entrypoints map[string]micheline.Prim `json:"entrypoints"`
+	}
+	eps := &eptype{}
+	err := c.Get(ctx, u, eps)
+	if err != nil {
+		return nil, err
+	}
+	return eps.Entrypoints, nil
+}
+
+// GetBigmapKeys returns all active keys in the bigmap id
+func (c *Client) GetBigmapKeys(ctx context.Context, id int64) ([]tezos.ExprHash, error) {
+	u := fmt.Sprintf("chains/%s/blocks/head/context/raw/json/big_maps/index/%d/contents", c.ChainID, id)
+	hashes := make([]tezos.ExprHash, 0)
+	err := c.Get(ctx, u, &hashes)
+	if err != nil {
+		return nil, err
+	}
+	return hashes, nil
+}
+
+// GetBigmapValue returns current active value at key hash from bigmap id
+func (c *Client) GetBigmapValue(ctx context.Context, id int64, hash tezos.ExprHash) (micheline.Prim, error) {
+	u := fmt.Sprintf("chains/%s/blocks/head/context/raw/json/big_maps/index/%d/contents/%s", c.ChainID, id, hash)
+	prim := micheline.Prim{}
+	err := c.Get(ctx, u, &prim)
+	if err != nil {
+		return micheline.InvalidPrim, err
+	}
+	return prim, nil
+}
+
+// GetBigmapValueHeight returns a value from bigmap id at key hash that was active at height
+func (c *Client) GetBigmapValueHeight(ctx context.Context, id int64, hash tezos.ExprHash, height int64) (micheline.Prim, error) {
+	u := fmt.Sprintf("chains/%s/blocks/%d/context/raw/json/big_maps/index/%d/contents/%s", c.ChainID, height, id, hash)
+	prim := micheline.Prim{}
+	err := c.Get(ctx, u, &prim)
+	if err != nil {
+		return micheline.InvalidPrim, err
+	}
+	return prim, nil
+}
+
+type BigmapInfo struct {
+	KeyType    micheline.Prim `json:"key_type"`
+	ValueType  micheline.Prim `json:"value_type"`
+	TotalBytes int64          `json:"total_bytes,string"`
+}
+
+// GetBigmapInfo returns type and content info from bigmap id
+func (c *Client) GetBigmapInfo(ctx context.Context, id int64) (*BigmapInfo, error) {
+	u := fmt.Sprintf("chains/%s/blocks/head/context/raw/json/big_maps/index/%d", c.ChainID, id)
+	info := &BigmapInfo{}
+	err := c.Get(ctx, u, info)
+	if err != nil {
+		return nil, err
+	}
+	return info, nil
 }
