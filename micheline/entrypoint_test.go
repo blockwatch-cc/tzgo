0fa0ca1af54ba94b1973352ce5c3bfe01636fb3c
Alexander Eichhorn <alex@blockwatch.cc>
2022-03-14 14:01:55 +0400

    Update entrypoint testcases

--- micheline/entrypoint_test.go
+++ micheline/entrypoint_test.go
@@ -23,7 +23,7 @@ var entryInfo = []entryTest{
 		Want: `{
 	        "default": {
 	          "branch": "/R",
-	          "call": "default",
+	          "name": "default",
 	          "id": 1,
 	          "type": [
 	            {
@@ -34,7 +34,7 @@ var entryInfo = []entryTest{
 	        },
 	        "do": {
 	          "branch": "/L",
-	          "call": "do",
+	          "name": "do",
 	          "id": 0,
 	          "type": [
                 {
@@ -66,14 +66,14 @@ var entryInfo = []entryTest{
 		Name: "FA2/HDAO",
 		Spec: `{"args":[{"args":[{"args":[{"args":[{"annots":["%balance_of"],"args":[{"annots":["%requests"],"args":[{"args":[{"annots":["%owner"],"prim":"address"},{"annots":["%token_id"],"prim":"nat"}],"prim":"pair"}],"prim":"list"},{"annots":["%callback"],"args":[{"args":[{"args":[{"annots":["%request"],"args":[{"annots":["%owner"],"prim":"address"},{"annots":["%token_id"],"prim":"nat"}],"prim":"pair"},{"annots":["%balance"],"prim":"nat"}],"prim":"pair"}],"prim":"list"}],"prim":"contract"}],"prim":"pair"},{"annots":["%hDAO_batch"],"args":[{"args":[{"annots":["%amount"],"prim":"nat"},{"annots":["%to_"],"prim":"address"}],"prim":"pair"}],"prim":"list"}],"prim":"or"},{"args":[{"annots":["%mint"],"args":[{"args":[{"annots":["%address"],"prim":"address"},{"annots":["%amount"],"prim":"nat"}],"prim":"pair"},{"args":[{"annots":["%token_id"],"prim":"nat"},{"annots":["%token_info"],"args":[{"prim":"string"},{"prim":"bytes"}],"prim":"map"}],"prim":"pair"}],"prim":"pair"},{"annots":["%set_administrator"],"prim":"address"}],"prim":"or"}],"prim":"or"},{"args":[{"args":[{"annots":["%set_pause"],"prim":"bool"},{"annots":["%token_metadata"],"args":[{"annots":["%token_ids"],"args":[{"prim":"nat"}],"prim":"list"},{"annots":["%handler"],"args":[{"args":[{"args":[{"annots":["%token_id"],"prim":"nat"},{"annots":["%token_info"],"args":[{"prim":"string"},{"prim":"bytes"}],"prim":"map"}],"prim":"pair"}],"prim":"list"},{"prim":"unit"}],"prim":"lambda"}],"prim":"pair"}],"prim":"or"},{"args":[{"annots":["%transfer"],"args":[{"args":[{"annots":["%from_"],"prim":"address"},{"annots":["%txs"],"args":[{"args":[{"annots":["%to_"],"prim":"address"},{"args":[{"annots":["%token_id"],"prim":"nat"},{"annots":["%amount"],"prim":"nat"}],"prim":"pair"}],"prim":"pair"}],"prim":"list"}],"prim":"pair"}],"prim":"list"},{"annots":["%update_operators"],"args":[{"args":[{"annots":["%add_operator"],"args":[{"annots":["%owner"],"prim":"address"},{"args":[{"annots":["%operator"],"prim":"address"},{"annots":["%token_id"],"prim":"nat"}],"prim":"pair"}],"prim":"pair"},{"annots":["%remove_operator"],"args":[{"annots":["%owner"],"prim":"address"},{"args":[{"annots":["%operator"],"prim":"address"},{"annots":["%token_id"],"prim":"nat"}],"prim":"pair"}],"prim":"pair"}],"prim":"or"}],"prim":"list"}],"prim":"or"}],"prim":"or"}],"prim":"or"}],"prim":"parameter"}`,
 		Want: `{
-			"balance_of":{"id":0,"call":"balance_of","branch":"/L/L/L","type":[{"name":"requests","type":"list","args":[{"name":"@item","type":"struct","args":[{"name":"owner","type":"address"},{"name":"token_id","type":"nat"}]}]},{"name":"callback","type":"contract","args":[{"name":"0","type":"list","args":[{"name":"@item","type":"struct","args":[{"name":"request","type":"struct","args":[{"name":"owner","type":"address"},{"name":"token_id","type":"nat"}]},{"name":"balance","type":"nat"}]}]}]}]},
-			"hDAO_batch":{"id":1,"call":"hDAO_batch","branch":"/L/L/R","type":[{"name":"","type":"list","args":[{"name":"@item","type":"struct","args":[{"name":"amount","type":"nat"},{"name":"to_","type":"address"}]}]}]},
-			"mint":{"id":2,"call":"mint","branch":"/L/R/L","type":[{"name":"address","type":"address"},{"name":"amount","type":"nat"},{"name":"token_id","type":"nat"},{"name":"token_info","type":"map","args":[{"name":"@key","type":"string"},{"name":"@value","type":"bytes"}]}]},
-			"set_administrator":{"id":3,"call":"set_administrator","branch":"/L/R/R","type":[{"name":"","type":"address"}]},
-			"set_pause":{"id":4,"call":"set_pause","branch":"/R/L/L","type":[{"name":"","type":"bool"}]},
-			"token_metadata":{"id":5,"call":"token_metadata","branch":"/R/L/R","type":[{"name":"token_ids","type":"list","args":[{"name":"@item","type":"nat"}]},{"name":"handler","type":"lambda","args":[{"name":"@param","type":"list","args":[{"name":"@item","type":"struct","args":[{"name":"token_id","type":"nat"},{"name":"token_info","type":"map","args":[{"name":"@key","type":"string"},{"name":"@value","type":"bytes"}]}]}]},{"name":"@return","type":"unit"}]}]},
-			"transfer":{"id":6,"call":"transfer","branch":"/R/R/L","type":[{"name":"","type":"list","args":[{"name":"@item","type":"struct","args":[{"name":"from_","type":"address"},{"name":"txs","type":"list","args":[{"name":"@item","type":"struct","args":[{"name":"to_","type":"address"},{"name":"token_id","type":"nat"},{"name":"amount","type":"nat"}]}]}]}]}]},
-			"update_operators":{"id":7,"call":"update_operators","branch":"/R/R/R","type":[{"name":"","type":"list","args":[{"name":"@item","type":"union","args":[{"name":"add_operator","type":"struct","args":[{"name":"owner","type":"address"},{"name":"operator","type":"address"},{"name":"token_id","type":"nat"}]},{"name":"remove_operator","type":"struct","args":[{"name":"owner","type":"address"},{"name":"operator","type":"address"},{"name":"token_id","type":"nat"}]}]}]}]}
+			"balance_of":{"id":0,"name":"balance_of","branch":"/L/L/L","type":[{"name":"requests","type":"list","args":[{"name":"@item","type":"struct","args":[{"name":"owner","type":"address"},{"name":"token_id","type":"nat"}]}]},{"name":"callback","type":"contract","args":[{"name":"0","type":"list","args":[{"name":"@item","type":"struct","args":[{"name":"request","type":"struct","args":[{"name":"owner","type":"address"},{"name":"token_id","type":"nat"}]},{"name":"balance","type":"nat"}]}]}]}]},
+			"hDAO_batch":{"id":1,"name":"hDAO_batch","branch":"/L/L/R","type":[{"name":"","type":"list","args":[{"name":"@item","type":"struct","args":[{"name":"amount","type":"nat"},{"name":"to_","type":"address"}]}]}]},
+			"mint":{"id":2,"name":"mint","branch":"/L/R/L","type":[{"name":"address","type":"address"},{"name":"amount","type":"nat"},{"name":"token_id","type":"nat"},{"name":"token_info","type":"map","args":[{"name":"@key","type":"string"},{"name":"@value","type":"bytes"}]}]},
+			"set_administrator":{"id":3,"name":"set_administrator","branch":"/L/R/R","type":[{"name":"","type":"address"}]},
+			"set_pause":{"id":4,"name":"set_pause","branch":"/R/L/L","type":[{"name":"","type":"bool"}]},
+			"token_metadata":{"id":5,"name":"token_metadata","branch":"/R/L/R","type":[{"name":"token_ids","type":"list","args":[{"name":"@item","type":"nat"}]},{"name":"handler","type":"lambda","args":[{"name":"@param","type":"list","args":[{"name":"@item","type":"struct","args":[{"name":"token_id","type":"nat"},{"name":"token_info","type":"map","args":[{"name":"@key","type":"string"},{"name":"@value","type":"bytes"}]}]}]},{"name":"@return","type":"unit"}]}]},
+			"transfer":{"id":6,"name":"transfer","branch":"/R/R/L","type":[{"name":"","type":"list","args":[{"name":"@item","type":"struct","args":[{"name":"from_","type":"address"},{"name":"txs","type":"list","args":[{"name":"@item","type":"struct","args":[{"name":"to_","type":"address"},{"name":"token_id","type":"nat"},{"name":"amount","type":"nat"}]}]}]}]}]},
+			"update_operators":{"id":7,"name":"update_operators","branch":"/R/R/R","type":[{"name":"","type":"list","args":[{"name":"@item","type":"union","args":[{"name":"add_operator","type":"struct","args":[{"name":"owner","type":"address"},{"name":"operator","type":"address"},{"name":"token_id","type":"nat"}]},{"name":"remove_operator","type":"struct","args":[{"name":"owner","type":"address"},{"name":"operator","type":"address"},{"name":"token_id","type":"nat"}]}]}]}]}
 		}`,
 	},
 
@@ -84,7 +84,7 @@ var entryInfo = []entryTest{
 		Want: `{
             "default": {
             	"branch": "",
-            	"call": "default",
+            	"name": "default",
             	"id": 0,
             	"type": [{"name":"","type":"address","optional":true}]
             }
