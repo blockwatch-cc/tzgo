710d07bbb0f7cea5b43b01cc272bc0de285e6160
Alexander Eichhorn <alex@blockwatch.cc>
2022-09-05 20:27:27 +0400

    Lint new file

--- rpc/increase_storage.go
+++ rpc/increase_storage.go
@@ -4,7 +4,7 @@
 package rpc
 
 import (
-    "blockwatch.cc/tzgo/tezos"
+	"blockwatch.cc/tzgo/tezos"
 )
 
 // Ensure IncreasePaidStorage implements the TypedOperation interface.
@@ -12,30 +12,30 @@ var _ TypedOperation = (*IncreasePaidStorage)(nil)
 
 // IncreasePaidStorage represents a transaction operation
 type IncreasePaidStorage struct {
-    Manager
-    Destination tezos.Address `json:"destination"`
-    Amount      int64         `json:"amount,string"`
+	Manager
+	Destination tezos.Address `json:"destination"`
+	Amount      int64         `json:"amount,string"`
 }
 
 // Costs returns operation cost to implement TypedOperation interface.
 func (t IncreasePaidStorage) Costs() tezos.Costs {
-    res := t.Metadata.Result
-    cost := tezos.Costs{
-        Fee:     t.Manager.Fee,
-        GasUsed: res.Gas(),
-    }
-    if !t.Result().IsSuccess() {
-        return cost
-    }
-    for _, v := range res.BalanceUpdates {
-        if v.Kind != CONTRACT {
-            continue
-        }
-        burn := v.Amount()
-        if burn >= 0 {
-            continue
-        }
-        cost.StorageBurn += -burn
-    }
-    return cost
+	res := t.Metadata.Result
+	cost := tezos.Costs{
+		Fee:     t.Manager.Fee,
+		GasUsed: res.Gas(),
+	}
+	if !t.Result().IsSuccess() {
+		return cost
+	}
+	for _, v := range res.BalanceUpdates {
+		if v.Kind != CONTRACT {
+			continue
+		}
+		burn := v.Amount()
+		if burn >= 0 {
+			continue
+		}
+		cost.StorageBurn += -burn
+	}
+	return cost
 }
