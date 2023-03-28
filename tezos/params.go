c8dcc5a6ade1fcf69dae09fc16aa8b84d7106095
Yukai Tu <yukai.tu@coinbase.com>
2023-03-28 11:20:50 -0700

    Mumbai Hardcode

--- tezos/params.go
+++ tezos/params.go
@@ -88,10 +88,19 @@ var (
 // with a custom data struct.
 type Params struct {
 	// identity
-	Network  string       `json:"network,omitempty"`
-	Version  int          `json:"version"`
-	ChainId  ChainIdHash  `json:"chain_id"`
-	Protocol ProtocolHash `json:"protocol"`
+	Name        string       `json:"name"`
+	Network     string       `json:"network,omitempty"`
+	Symbol      string       `json:"symbol"`
+	Deployment  int          `json:"deployment"`
+	Version     int          `json:"version"`
+	ChainId     ChainIdHash  `json:"chain_id"`
+	Protocol    ProtocolHash `json:"protocol"`
+	StartHeight int64        `json:"start_height"`
+	EndHeight   int64        `json:"end_height"`
+	Decimals    int          `json:"decimals"`
+	Token       int64        `json:"units"` // atomic units per token
+
+	EndorsementReward int64 `json:"endorsement_reward"`
 
 	// timing
 	MinimalBlockDelay time.Duration `json:"minimal_block_delay"`
@@ -101,16 +110,31 @@ type Params struct {
 	OriginationSize int64 `json:"origination_size"`
 
 	// limits
+	BlockReward                  int64 `json:"block_reward"`
 	BlocksPerCycle               int64 `json:"blocks_per_cycle"`
+	EndorsersPerBlock            int   `json:"endorsers_per_block"`
 	PreservedCycles              int64 `json:"preserved_cycles"`
 	HardGasLimitPerOperation     int64 `json:"hard_gas_limit_per_operation"`
 	HardGasLimitPerBlock         int64 `json:"hard_gas_limit_per_block"`
 	HardStorageLimitPerOperation int64 `json:"hard_storage_limit_per_operation"`
 	MaxOperationDataLength       int   `json:"max_operation_data_length"`
 	MaxOperationsTTL             int64 `json:"max_operations_ttl"`
+	BlocksPerCommitment          int64 `json:"blocks_per_commitment"`
+	BlocksPerSnapshot            int64 `json:"blocks_per_snapshot"`
+	ConsensusCommitteeSize       int   `json:"consensus_committee_size"`
+
+	BlocksPerVotingPeriod int64 `json:"blocks_per_voting_period"`
+	CyclesPerVotingPeriod int64 `json:"cycles_per_voting_period"`
+	MinProposalQuorum     int64 `json:"min_proposal_quorum"`
+	QuorumMin             int64 `json:"quorum_min"`
+	QuorumMax             int64 `json:"quorum_max"`
 
 	// extra features to follow protocol upgrades
-	OperationTagsVersion int `json:"operation_tags_version,omitempty"` // 1 after v005
+	OperationTagsVersion int   `json:"operation_tags_version,omitempty"` // 1 after v005
+	NumVotingPeriods     int   `json:"num_voting_periods,omitempty"`     // 5 after v008, 4 before
+	StartBlockOffset     int64 `json:"start_block_offset,omitempty"`     // correct start/end cycle since Granada
+	StartCycle           int64 `json:"start_cycle,omitempty"`            // correction since Granada v10
+	VoteBlockOffset      int64 `json:"vote_block_offset,omitempty"`      // correction for Edo + Florence Mainnet-only +1 bug
 }
 
 func (p *Params) WithChainId(id ChainIdHash) *Params {
@@ -141,4 +165,201 @@ func (p Params) SnapshotBaseCycle(cycle int64) int64 {
 		offset = 1
 	}
 	return cycle - (p.PreservedCycles + offset)
+}
+
+func (p *Params) IsCycleStart(height int64) bool {
+	pp := p
+	if !p.ContainsHeight(height) {
+		pp = p.ForHeight(height)
+	}
+	return height > 0 && (height-pp.StartBlockOffset-1)%pp.BlocksPerCycle == 0
+}
+
+func (p *Params) IsCycleEnd(height int64) bool {
+	pp := p
+	if !p.ContainsHeight(height) {
+		pp = p.ForHeight(height)
+	}
+	return height > 0 && (height-pp.StartBlockOffset)%pp.BlocksPerCycle == 0
+}
+
+func (p *Params) IsSnapshotBlock(height int64) bool {
+	pp := p
+	if !p.ContainsHeight(height) {
+		pp = p.ForHeight(height)
+	}
+	return (height-pp.StartBlockOffset)%pp.SnapshotBlocks() == 0
+}
+
+func (p *Params) IsSeedRequired(height int64) bool {
+	pp := p
+	if !p.ContainsHeight(height) {
+		pp = p.ForHeight(height)
+	}
+	return (height-pp.StartBlockOffset)%pp.BlocksPerCommitment == 0
+}
+
+func (p *Params) CycleFromHeight(height int64) int64 {
+	if height == 0 {
+		return 0
+	}
+	correct := int64(0)
+	if p.StartBlockOffset == height {
+		correct = 1
+	}
+	pp := p
+	if !p.ContainsHeight(height) {
+		pp = p.ForHeight(height)
+	}
+	return pp.StartCycle + (height-pp.StartBlockOffset-1)/pp.BlocksPerCycle - correct
+}
+
+func (p *Params) CycleStartHeight(cycle int64) int64 {
+	pp := p
+	if !p.ContainsCycle(cycle) {
+		pp = p.ForCycle(cycle)
+	}
+	return pp.StartBlockOffset + (cycle-pp.StartCycle)*pp.BlocksPerCycle + 1
+}
+
+func (p *Params) CycleEndHeight(cycle int64) int64 {
+	pp := p
+	if !p.ContainsCycle(cycle) {
+		pp = p.ForCycle(cycle)
+	}
+	return pp.StartBlockOffset + (cycle-pp.StartCycle+1)*pp.BlocksPerCycle
+}
+
+func (p *Params) CyclePosition(height int64) int64 {
+	pp := p
+	if !p.ContainsHeight(height) {
+		pp = p.ForHeight(height)
+	}
+	return height - pp.CycleStartHeight(pp.CycleFromHeight(height))
+}
+
+func (p *Params) SnapshotBlock(cycle int64, index int) int64 {
+	base := p.SnapshotBaseCycle(cycle)
+	if base < 0 {
+		return 0
+	}
+	pp := p
+	if !p.ContainsCycle(base) {
+		pp = p.ForCycle(base)
+	}
+	return pp.CycleStartHeight(base) + int64(index+1)*pp.SnapshotBlocks() - 1
+}
+
+func (p *Params) SnapshotIndex(height int64) int {
+	pp := p
+	if !p.ContainsHeight(height) {
+		pp = p.ForHeight(height)
+	}
+	if height == pp.StartBlockOffset {
+		return int(pp.BlocksPerCycle/pp.SnapshotBlocks() - 1)
+	}
+	return int(((height - pp.StartBlockOffset - pp.SnapshotBlocks()) % pp.BlocksPerCycle) / pp.SnapshotBlocks())
+}
+
+func (p *Params) SnapshotBlocks() int64 {
+	return p.BlocksPerSnapshot
+}
+
+func (p *Params) MaxSnapshotIndex() int64 {
+	return (p.BlocksPerCycle / p.SnapshotBlocks()) - 1
+}
+
+func (p *Params) VotingStartCycleFromHeight(height int64) int64 {
+	pp := p
+	if !p.ContainsHeight(height) {
+		pp = p.ForHeight(height)
+	}
+	// Edo voting bug does not apply to first Edo block
+	offs := pp.VoteBlockOffset
+	if height == pp.StartBlockOffset+1 {
+		offs = 0
+	}
+	currentCycle := pp.CycleFromHeight(height + offs)
+	offset := (height + offs - pp.StartBlockOffset - 1) % pp.BlocksPerVotingPeriod
+	return currentCycle - offset/pp.BlocksPerCycle
+}
+
+func (p *Params) IsVoteStart(height int64) bool {
+	pp := p
+	if !p.ContainsHeight(height) {
+		pp = p.ForHeight(height)
+	}
+	// Edo voting bug does not apply to first Edo block
+	offs := pp.VoteBlockOffset
+	if height == pp.StartBlockOffset+1 {
+		offs = 0
+	}
+	return height > 0 && (height-pp.StartBlockOffset-1+offs)%pp.BlocksPerVotingPeriod == 0
+}
+
+func (p *Params) IsVoteEnd(height int64) bool {
+	pp := p
+	if !p.ContainsHeight(height) {
+		pp = p.ForHeight(height)
+	}
+	offs := pp.VoteBlockOffset
+	return height > 0 && (height-pp.StartBlockOffset+offs)%pp.BlocksPerVotingPeriod == 0
+}
+
+func (p *Params) VoteStartHeight(height int64) int64 {
+	pp := p
+	if !p.ContainsHeight(height) {
+		pp = p.ForHeight(height)
+	}
+	// Edo voting bug does not apply to first Edo block
+	offs := pp.VoteBlockOffset
+	if height == pp.StartBlockOffset+1 {
+		offs = 0
+	}
+	return pp.CycleStartHeight(pp.VotingStartCycleFromHeight(height)) - offs
+}
+
+func (p *Params) VoteEndHeight(height int64) int64 {
+	pp := p
+	if !p.ContainsHeight(height) {
+		pp = p.ForHeight(height)
+	}
+	return pp.VoteStartHeight(height) + pp.BlocksPerVotingPeriod - 1
+}
+
+func (p *Params) MaxBlockReward() int64 {
+	if p.Version < 12 {
+		return p.BlockReward + p.EndorsementReward*int64(p.EndorsersPerBlock)
+	}
+	return p.BlockReward + p.EndorsementReward*int64(p.ConsensusCommitteeSize)
+}
+
+func (p *Params) ContainsHeight(height int64) bool {
+	// treat -1 as special height query that matches open interval params only
+	return (height < 0 && p.EndHeight < 0) ||
+		(p.StartHeight <= height && (p.EndHeight < 0 || p.EndHeight >= height))
+}
+
+func (p *Params) ContainsCycle(cycle int64) bool {
+	return p.StartCycle == 0 || p.StartCycle <= cycle
+}
+
+func (p *Params) IsMainnet() bool {
+	return p.ChainId.Equal(Mainnet)
+}
+
+func (p *Params) IsPostBabylon() bool {
+	return p.IsMainnet() && p.Version >= 5
+}
+
+func (p *Params) IsPreBabylonHeight(height int64) bool {
+	return p.IsMainnet() && height < 655360
+}
+
+func (p *Params) BlockTime() time.Duration {
+	return p.MinimalBlockDelay
+}
+
+func (p *Params) NumEndorsers() int {
+	return p.EndorsersPerBlock + p.ConsensusCommitteeSize
 }
