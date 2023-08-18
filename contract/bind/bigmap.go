d314c46fcabb3587c705a2f0c17a5d64327520af
Abdul-Harisu Inusah <28383750+ankorGH@users.noreply.github.com>
2023-08-18 10:26:03 +0000

    add support for marshalling generated bigmap using bind

--- contract/bind/bigmap.go
+++ contract/bind/bigmap.go
@@ -1,12 +1,13 @@
 package bind
 
 import (
-	"blockwatch.cc/tzgo/micheline"
-	"blockwatch.cc/tzgo/rpc"
 	"context"
 	"fmt"
-	"github.com/pkg/errors"
 	"strconv"
+
+	"blockwatch.cc/tzgo/micheline"
+	"blockwatch.cc/tzgo/rpc"
+	"github.com/pkg/errors"
 )
 
 // Bigmap is a handle to a Tezos bigmap.
@@ -19,6 +20,7 @@ type Bigmap[K, V any] struct {
 	id      int64
 	keyType *micheline.Type
 	rpc     RPC
+	m       []MapEntry[K, V]
 }
 
 // NewBigmap returns a new Bigmap that points to the given bigmap id.
@@ -32,6 +34,10 @@ func NewBigmap[K, V any](id int64) Bigmap[K, V] {
 // ID returns the id of the bigmap.
 func (b *Bigmap[K, B]) ID() int64 {
 	return b.id
+}
+
+func (b *Bigmap[K, V]) SetContent(elt ...MapEntry[K, V]) {
+	b.m = elt
 }
 
 // SetRPC defines the client to use when getting a value from the bigmap.
@@ -98,8 +104,48 @@ func (b Bigmap[K, V]) String() string {
 	return "Bigmap#" + strconv.Itoa(int(b.id))
 }
 
+func (b Bigmap[K, V]) MarshalPrim(optimized bool) (micheline.Prim, error) {
+	entries := make([]micheline.Prim, 0, len(b.m))
+	for _, value := range b.m {
+		keyPrim, err := MarshalPrim(value.Key, optimized)
+		if err != nil {
+			return micheline.Prim{}, errors.Wrap(err, "failed to marshal key")
+		}
+		valuePrim, err := MarshalPrim(value.Value, optimized)
+		if err != nil {
+			return micheline.Prim{}, errors.Wrap(err, "failed to marshal value")
+		}
+		entries = append(entries, micheline.NewCode(micheline.D_ELT, keyPrim, valuePrim))
+	}
+	return micheline.NewSeq(entries...), nil
+}
+
 func (b *Bigmap[K, V]) UnmarshalPrim(prim micheline.Prim) error {
-	*b = NewBigmap[K, V](prim.Int.Int64())
+	if prim.Int != nil {
+		*b = NewBigmap[K, V](prim.Int.Int64())
+		return nil
+	}
+	if prim.Type != micheline.PrimSequence {
+		return fmt.Errorf("not supported type for unmarshall")
+	}
+	*b = NewBigmap[K, V](0)
+	for _, entry := range prim.Args {
+		if entry.OpCode != micheline.D_ELT {
+			return errors.Errorf("bigmap entries should be ELT, got %s", prim.OpCode)
+		}
+		if len(entry.Args) != 2 {
+			return errors.New("prim ELT should have 2 args")
+		}
+		var key K
+		var value V
+		if err := UnmarshalPrim(entry.Args[0], &key); err != nil {
+			return errors.Wrap(err, "failed to unmarshal key")
+		}
+		if err := UnmarshalPrim(entry.Args[1], &value); err != nil {
+			return errors.Wrap(err, "failed to unmarshal value")
+		}
+		b.m = append(b.m, MapEntry[K, V]{Key: key, Value: value})
+	}
 	return nil
 }
 
